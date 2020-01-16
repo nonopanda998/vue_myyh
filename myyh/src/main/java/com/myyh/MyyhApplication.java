23c98492b4cde3f1c2eb67f7ee4c390eb5847ddd
859946068@qq.com <Zh718294>
2020-01-16 09:14:16 +0800

    基础调整

---
+++ myyh/src/main/java/com/myyh/MyyhApplication.java
@@ -0,0 +1,13 @@
+package com.myyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyyhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyyhApplication.class, args);
+    }
+
+}
